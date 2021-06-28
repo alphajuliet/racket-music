b9eb3f6991c54aa13d86942bdd81d58eb7fd8246
alphajuliet <andrew@alphajuliet.com>
2021-06-28 22:10:45 +1000

    Define new note/number conversions that handle multiple note names. Add collapse function. Define wrappers that use the new conversions. Simplify calls with improved FP definitions and curried functions. Streamline and simplify function calls. Reformat code.

--- music-test.rkt
+++ music-test.rkt
@@ -1,6 +1,6 @@
 #lang racket
-; music-test.rkt
-; Andrew 2018-06-24
+;; music-test.rkt
+;; Andrew 2018-06-24
 
 (require rackunit
          rackunit/text-ui
@@ -8,38 +8,49 @@
 
 (define-test-suite music-tests
 
-  ; Utilities
+  ;; Utilities
   (test-case "Sanity"
              (check-equal? (+ 2 2) 4))
+
   (test-case "map+"
              (check-equal? (map+ add1 '(1 2 3)) '(2 3 4))
              (check-equal? (map+ add1 4) 5)
-             (define f (map+ add1))
-             (check-equal? (f 4) 5 ))
+             (define inc (map+ add1))
+             (check-equal? (inc 4) 5 ))
+
   (test-case "list-min"
              (check-equal? (list-min '(2 1 3)) 1))
 
-  ; Musical utilities
+  ;; Musical utilities
   (test-case "transpose"
              (check-equal? (transpose 4 6) 10)
              (check-equal? (transpose 10 5) 3)
              (check-equal? (transpose 8 '(0 4 7)) '(8 0 3)))
+
   (test-case "canonical"
              (check-equal? (canonical '(6 2 8)) '(0 4 6)))
+
   (test-case "num->note"
              (check-equal? (num->note 2) 'D)
-             (check-equal? (num->note '(0 2 4)) '(C D E)))
+             (check-equal? (num->note '(0 2 4)) '(C D E))
+             (check-equal? (num->note* '(1 3 4)) '((C# Db) (D# Eb) (E))))
+
   (test-case "note->num"
              (check-equal? (note->num 'E) 4)
-             (check-equal? (note->num '(C D E)) '(0 2 4)))
+             (check-equal? (note->num '(C D E)) '(0 2 4))
+             (check-equal? (note->num* 'Ab) 8))
+
+  (test-case "collapse"
+             (define x (num->note* '(0 3 8)))
+             (check-equal? (collapse 'Db x) '(C Eb Ab))
+             (check-equal? (collapse 'C# x) '(C D# G#)))
+
   (test-case "sorted-num->note"
              (check-equal? (sorted-num->note '(4 2 0)) '(C D E)))
 
-  ; Chords
-  (test-case "sc"
-             (check-equal? (sc 'F maj2) '(F G A C)))
   (test-case "Wrapped functions"
-             (check-equal? (tr* 4 (sc 'F# maj7)) '(Bb D F A)))
+             (check-equal? (wrap* (transpose 2) '(C E G)) '(D F# A))
+             (check-equal? (wrap* maj2 'G) '(G A B D)))
   
   )
 
--- music.rkt
+++ music.rkt
@@ -1,91 +1,152 @@
 #lang racket
-; music.rkt
-; Define musical functions
-; AJ 2017-04-28
+;; music.rkt
+;; Define musical functions
+;; AJ 2017-04-28
 
 (require rsound
          threading
+         define-with-spec
          rakeda)
 
 (provide (all-defined-out))
 
-;-----------------------
-; Utility functions
+;;-----------------------
+;; Utility functions
 
-; Generic map that is polymorphic across lists and values
-; map+ :: ([a] → [b]) → [a] → [b]
-; map+ :: ([a] → [b]) → a → b
 (define/c (map+ f x)
+  ;; Generic map that is polymorphic across lists and values
+  ;; map+ :: ([a] → [b]) → [a] → [b]
+  ;; map+ :: ([a] → [b]) → a → b
   (if (list? x)
       (r:map f x)
       (apply f (list x))))
 
-; sort-nums :: Sortable a ⇒ [a] → [a]
-(define sort-nums (r:sort <))
+(define sort-nums
+  ;; sort-nums :: Sortable a ⇒ [a] → [a]
+  (map+ (r:sort <)))
 
-; Minimum of a list
-; list-min :: Sortable a ⇒ [a] → a
-(define list-min (curry argmin identity))
+(define list-min
+  ;; Minimum of a list
+  ;; list-min :: Sortable a ⇒ [a] → a
+  (map+ (r:argmin identity)))
 
-;-----------------------
-(define mod12 (r:flip modulo 12))
-(define/c (transpose a b)
-  (map+ (compose mod12 (r:add a)) b))
-(define/c (invert a b)
-  (map+ (compose mod12 (r:subtract a)) b))
+(define mod12
+  ;; Modulo 12
+  (r:flip modulo 12))
 
-; Canonical structure of a list
-; e.g (canonical '(2 6 8)) -> '(0 4 6), i.e. this is a major chord
-; canonical :: Sortable a ⇒ [a] → [a]
+;;-----------------------
+(define/c (transpose n x)
+  ;; Transpose a note or chord, modulo 12
+  ;; e.g. (transpose 2 '(0 3 7)) => '(2 5 9)
+  (map+ (compose mod12 (r:add n)) x))
+
+(define/c (invert n x)
+  ;; Invert modulo 12
+  (map+ (compose mod12 (r:subtract n)) x))
+
 (define (canonical ch)
-  (sort-nums (map+ (transpose (- (list-min ch))) ch)))
+  ;; Canonical structure of a list
+  ;; e.g (canonical '(2 6 8)) -> '(0 4 6), i.e. this is a major chord
+  ;; canonical :: Sortable a ⇒ [a] → [a]
+  (sort-nums
+   (map+ (transpose (- (list-min ch))) ch)))
 
-;-----------------------
-; Musical data structures and conversions
+;;-----------------------
+;; Musical data structures and conversions
 
-; Define the scale and standard note names assuming C as root
-; scale :: [Integer]
-(define scale (range 0 11))
+(define scale
+  ;; scale :: [Integer]
+  (range 0 12))
 
-; Note names, using my favourite mix of sharps and flats
-; define NoteName = Symbol
-; noteNames :: [NoteNames]
-(define noteNames `(C C# D Eb E F F# G Ab A Bb B))
+(define noteNames
+  ;; Note names, using my favourite mix of sharps and flats
+  ;; define NoteName = Symbol
+  ;; noteNames :: [NoteNames]
+  `(C C# D Eb E F F# G Ab A Bb B))
 
-; Polymorphic conversions between note numbers and note names
-; num->note :: [Integer] → [NoteName] || Integer → NoteName
 (define num->note
-  (map+ (r:flip r:nth noteNames)))
+  ;; Polymorphic conversions between note numbers and note names
+  ;; num->note :: [Integer] → [NoteName] || Integer → NoteName
+  (map+ (compose (r:flip r:nth noteNames) mod12)))
 
-; note->num :: [NoteName] → [Integer] || NoteName → Integer
+;; note->num :: [NoteName] → [Integer] || NoteName → Integer
 (define note->num
   (map+ (r:flip r:index-of noteNames)))
 
-; Sorted version of num->note
-; sorted-num->note :: [Integer] → [NoteName]
+;;-----------------------
+(define allNotes
+  ;; Comprehensive list of note names
+  '((C) (C# Db) (D) (D# Eb) (E) (F) (F# Gb) (G) (G# Ab) (A) (A# Bb) (B)))
+
+(define num->note*
+  ;; num->note* :: [Integer] → [[NoteName]] || Integer → [NoteName]
+  (map+ (compose (r:flip r:nth allNotes) mod12)))
+
+(define (note->num* n)
+  ;; note->num* :: NoteName → Integer || [NoteName] → [Integer]
+  (map+ (λ (e) (r:index-where (curry true?) (map (curry r:index-of e) allNotes))) n))
+
+(define/c (collapse note lst)
+  ;; Collapse a list of note lists based on the given note
+  ;; C# -> D#, F#..., but Db -> Eb, Gb...
+  ;; e.g. (collapse 'Eb '((C# Db) (F# Gb) (B))) => '(Db Gb B)
+  (let* ([base-note (symbol->string note)])
+    (if (and (= 2 (string-length base-note))
+             (eq? #\b (string-ref base-note 1)))
+        (map last lst)
+        (map first lst))))
+ 
+;;-----------------------
+;; Define wrappers around functions that transform note numbers
+
+(define/c (wrap f x)
+  ((compose num->note f note->num) x))
+
 (define sorted-num->note
+  ;; Sorted version of num->note
+  ;; sorted-num->note :: [Integer] → [NoteName]
   (compose num->note sort-nums))
 
-; Define wrappers around functions that transform note numbers
-(define (wrap f)
-  (compose num->note f note->num))
-(define (wrap* f)
-  (compose sorted-num->note f note->num))
+(define/c (wrap-sorted f x)
+  ((compose sorted-num->note f note->num) x))
+
 (define (wrap2 f x y)
   (~>> y
        note->num
        (map+ (curry f x))
        num->note))
 
-;-----------------------
-; Define a chord as a list of intervals from the base note. It returns a
-; function that takes a given base note number and returns the chord of note numbers.
-; define Chord = Integer → [Integer]
-; chord :: [Integer] → Chord
-(define (chord ns)
-  (λ (n) (r:map (curry transpose n) ns)))
+;;-----------------------
+;; Define better wrappers
 
-; Define chords as functions operating on a note number
+(define/c (wrap* f x)
+  ;; Wrap note->num* and num->note*
+  ;; e.g. (wrap* (transpose 2) '(C D E)) => '(D E F#)
+  ;;      (wrap* maj2 'G) => '(G A B D)
+  ((compose (collapse 'C)
+            num->note*
+            f
+            note->num*) x))
+
+(define/c (wrapl* f x)
+  ;; Wrap a function that returns a list
+  ;; e.g. (wrap* inversions '(C E G)) => '((G C E) (C E G) (E G C))
+  ((compose (r:map (collapse 'C))
+            (r:map num->note*)
+            f
+            note->num*) x))
+
+;;-----------------------
+;; Define a chord as a list of intervals from the base note. It returns a
+;; function that takes a given base note number and returns the chord of note numbers.
+;; define Chord = Integer → [Integer]
+
+(define (chord ns)
+  ;; chord :: [Integer] → Chord
+  ;; e.g. (maj6 4) => '(4 8 1)
+  (λ (n) (r:map (transpose n) ns)))
+
+;; Define chords as functions operating on a note number
 (define-syntax-rule (defchord name ns)
   (define name (chord ns)))
 
@@ -106,53 +167,40 @@
 (defchord min2    '(0 2 3 7))
 (defchord dim     '(0 3 6))
 
-(define chord-defns
-  (hash 'major '(0 4 7)
-        'minor '(0 3 7)
-        'x4+5  '(0 5 7)))
+(define main-chords
+  ;; main-chords :: [Chord]
+  (list major minor x7 maj7 min7))
 
-; main-chords :: [Chord]
-(define main-chords (list major minor x7 maj7 min7))
+(define show-main-chords
+  ;; Show all the main chords
+  ;; e.g. (show-main-chords 'F#) => '((F# A# C#) (F# A C#) (F# A# C# E) (F# A# C# F) (F# A C# E))
+  (wrapl* (r:juxt main-chords)))
 
-; Show a list of chords for a given note
-; apply-map :: [Chord] → NoteName → [NoteName]
-(define (apply-map fns lst)
-  (r:map (λ (f) (f lst)) fns))
-
-(define (show-chords ch nt)
-  (r:map num->note (apply-map ch (note->num nt))))
-
-; Match note numbers to one or more chord names
-; This will do a match across all the given chords
-; @@TODO
 (define (match-chord lst)
+  ;; Match note numbers to one or more chord names
+  ;; This will do a match across all the given chords
+  ;; @@TODO
   (let ([x (map canonical (permutations lst))])
     #f))
 
-; Show all inversions of a chord
 (define (inversions ch)
+  ;; Show all inversions of a chord
   (r:nest-list r:rotate-left (length ch) ch))
 
-;----------------
-; Friendly definitions for use
+;;----------------
+;; Example usage of the above functions
 
-(define/c (tr* n nt) (wrap2 transpose n nt))
-(define/c (inv* n nt) (wrap2 invert n nt))
+;; (wrap* (compose (transpose 2) min7) 'G)
+;; (wrapl* (compose inversions min6 'E))
+;; (show-main-chords 'D)
 
-; Show chord
-; sc :: NoteName → Chord → [NoteName]
-(define/c (sc root ch)
-  ((wrap ch) root))
-
-; e.g. (tr* 4 (sc 'F# maj7)) -> '(Bb D F A)
-
-;----------------
-; Define the PLR group of functions on triads from Neo-Riemannian Theory
-; (see Wikipedia entry at https://en.wikipedia.org/wiki/Neo-Riemannian_theory).
-; - P is an involution (i.e. a permutation with maximum cycle length 2) between the major and
-;   minor triad of the same root: P(Cmaj) = Cmin
-; - R is an involution between relative major and minor triads: R(Cmaj) = Amin
-; - L doesn't have a recognised musical mapping: L(Cmaj) = Emin
+;;----------------
+;; Define the PLR group of functions on triads from Neo-Riemannian Theory
+;; (see Wikipedia entry at https://en.wikipedia.org/wiki/Neo-Riemannian_theory).
+;; - P is an involution (i.e. a permutation with maximum cycle length 2) between the major and
+;;   minor triad of the same root: P(Cmaj) = Cmin
+;; - R is an involution between relative major and minor triads: R(Cmaj) = Amin
+;; - L doesn't have a recognised musical mapping: L(Cmaj) = Emin
 
 (define (P triad)
   (r:map (invert (r:add (r:nth 0 triad)
@@ -173,10 +221,10 @@
 (define S (compose R P L))
 (define H (compose L P L))
 
-; Define wrapped versions
-; e.g. (P* (sc 'C maj)) → '(C Eb G)
-; P* :: [NoteName] → [NoteName]
-; etc.
+;; Define wrapped versions
+;; e.g. (P* (sc 'C maj)) → '(C Eb G)
+;; P* :: [NoteName] → [NoteName]
+;; etc.
 (define P* (wrap* P))
 (define L* (wrap* L))
 (define R* (wrap* R))
@@ -184,7 +232,7 @@
 (define S* (wrap* S))
 (define H* (wrap* H))
 
-;----------------
+;;----------------
 (displayln "Loaded musical definitions.")
 
-; The End
+;; The End
